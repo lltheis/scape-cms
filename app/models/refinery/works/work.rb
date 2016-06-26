13fe0b98273a0664d7e517a78c0d591b36374dc5
lltheis <theis.laura.l@gmail.com>
2016-06-26 23:15:39 +0000

    override all model, view and controllers to allow for customization

---
+++ app/models/refinery/works/work.rb
@@ -0,0 +1,31 @@
+module Refinery
+  module Works
+    class Work < Refinery::Core::BaseModel
+      self.table_name = 'refinery_works'
+
+
+      validates :name, :presence => true, :uniqueness => true
+
+      belongs_to :img_1, :class_name => '::Refinery::Image'
+
+      belongs_to :img_2, :class_name => '::Refinery::Image'
+
+      belongs_to :img_3, :class_name => '::Refinery::Image'
+
+      belongs_to :img_4, :class_name => '::Refinery::Image'
+
+      belongs_to :img_5, :class_name => '::Refinery::Image'
+
+      belongs_to :img_6, :class_name => '::Refinery::Image'
+
+      belongs_to :img_7, :class_name => '::Refinery::Image'
+
+      belongs_to :img_8, :class_name => '::Refinery::Image'
+
+      # To enable admin searching, add acts_as_indexed on searchable fields, for example:
+      #
+      #   acts_as_indexed :fields => [:title]
+
+    end
+  end
+end
